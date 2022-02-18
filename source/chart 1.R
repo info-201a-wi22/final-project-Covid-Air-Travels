2bd6cd0f7caa5441a3bf0a8419eeddf7f9712db9
lattematics <97063743+lattematics@users.noreply.github.com>
2022-02-18 12:43:52 -0800

    fixed labels

--- source/chart 1.R
+++ source/chart 1.R
@@ -27,7 +27,10 @@ mar_total <- airlines %>%
   group_by(Year, Month) %>% 
   summarise(Total = mean(Total))
 
-chart1 <- ggplot(data = NULL, aes(x = Year, y = Total, color = Month)) +
+chart1 <- ggplot(data = NULL, aes(x = Year, y = Total,color = factor(Month))) +
   geom_line(data = jan_total) + 
   geom_line(data = feb_total) + 
-  geom_line(data = mar_total)
+  geom_line(data = mar_total) +
+  ggtitle("Total Passengers by Months") +
+  ylab("Total Passenger") +
+  scale_color_discrete(name = "Months", label = c("January", "February", "March"))
