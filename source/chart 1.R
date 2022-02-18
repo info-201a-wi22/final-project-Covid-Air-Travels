044b375ed8bda1f4b2aa71d82cb8975f4e52792f
lattematics <linda71@2uw.edu>
2022-02-18 11:55:59 -0800

    updated chart 1

--- source/chart 1.R
+++ source/chart 1.R
@@ -12,23 +12,22 @@ airlines <- read.csv(
 jan_total <- airlines %>%
   select(Year, Month, Total) %>%
   filter(Month == 1) %>%
-  group_by(Year) %>%
+  group_by(Year, Month) %>%
   summarise(Total = mean(Total))
 
 feb_total <- airlines %>%
   select(Year, Month, Total) %>%
   filter(Month == 2) %>%
-  group_by(Year) %>%
+  group_by(Year, Month) %>%
   summarise(Total = mean(Total))
 
 mar_total <- airlines %>%
   select(Year, Month, Total) %>%
   filter(Month == 3) %>%
-  group_by(Year) %>% 
+  group_by(Year, Month) %>% 
   summarise(Total = mean(Total))
 
-ggplot(data = NULL, aes(x = Year, y = Total)) +
-  geom_line(data = jan_total, color = "green") + 
-  geom_line(data = feb_total, color = "purple") + 
-  geom_line(data = mar_total, color = "orange")
-
+chart1 <- ggplot(data = NULL, aes(x = Year, y = Total, color = Month)) +
+  geom_line(data = jan_total) + 
+  geom_line(data = feb_total) + 
+  geom_line(data = mar_total)
