0185dcf90678061fd7f4b46035fb04c4a257d453
johnjuneho <jho12@uw.edu>
2022-02-18 00:42:08 -0800

    Summary Table Final

--- source/summary table.R
+++ source/summary table.R
@@ -1,1 +1,29 @@
-# A file that creates a table of summary information to be included in your report
+# Load packages
+library(dplyr)
+library(tidyverse)
+
+# Set Working Directory
+setwd("~/Documents/INFO_201/final-project-Covid_Airtravel-/source")
+
+# Import Data 
+International_Report_Passengers <- read.csv(
+  "../Data/International_Report_Passengers.csv",
+  header = TRUE,
+  stringsAsFactors = FALSE
+)
+
+# International Report Passengers Dataframe Manipulation 
+Passengers_Report <- International_Report_Passengers %>% 
+  filter(Month <= 3) %>% 
+  group_by(Year) %>% 
+  summarize(total_passengers = sum(Total)) %>% 
+  mutate(percent_total = round((total_passengers / sum(total_passengers) * 100), 2)) %>% 
+  mutate(shift = lead(Passengers_Report$total_passengers, default = 0)) %>% 
+  mutate(percent_increase = round(((shift - total_passengers) / total_passengers) * 100, 2)) %>% 
+  mutate(percent_increase = lag(Passengers_Report$percent_increase, default = NA)) %>% 
+  select(-shift) %>% 
+  rename(janurary_to_march_each_year = Year)
+
+
+
+
