9f42c0f1c22c284ee29323d34ad5e3ef6583f27b
lattematics <97063743+lattematics@users.noreply.github.com>
2022-02-18 12:46:52 -0800

    changed spacing

--- source/chart 1.R
+++ source/chart 1.R
@@ -27,10 +27,12 @@ mar_total <- airlines %>%
   group_by(Year, Month) %>% 
   summarise(Total = mean(Total))
 
-chart1 <- ggplot(data = NULL, aes(x = Year, y = Total,color = factor(Month))) +
+chart1 <- ggplot(data = NULL, 
+                 aes(x = Year, y = Total,color = factor(Month))) +
   geom_line(data = jan_total) + 
   geom_line(data = feb_total) + 
   geom_line(data = mar_total) +
   ggtitle("Total Passengers by Months") +
   ylab("Total Passenger") +
-  scale_color_discrete(name = "Months", label = c("January", "February", "March"))
+  scale_color_discrete(name = "Months", 
+                       label = c("January", "February", "March"))
