3e98c4a7c56c2b634266087634f94cd1aaa6d10a
johnjuneho <jho12@uw.edu>
2022-02-18 00:47:07 -0800

    Summary Table Final

--- source/summary.R
+++ source/summary.R
@@ -1,1 +1,18 @@
 # A file that calculates summary information to be included in your report
+passengers_report <- read.csv("~/INFO201/final-project-Covid-Air-Travels/data/International_Report_Passengers.csv")
+View(passengers_report)
+
+
+# 1.
+
+
+# 2. How much did the total number of passengers decline when covid hit (2019 - 2020)?
+
+
+# 3. 
+
+
+# 4.
+
+
+# 5.
