89ef049e1de40d9afbe838c4f5f7e84ddad39e9f
johnjuneho <jho12@uw.edu>
2022-02-18 22:15:22 -0800

    Report summary table description

---
+++ source/summary table.R
@@ -0,0 +1,40 @@
+# Load packages
+library(dplyr)
+library(tidyverse)
+
+# Set Working Directory
+setwd("~/Documents/INFO_201/final-project-Covid_Airtravel-/source")
+
+# Import Data 
+International_Report_Passengers <- read.csv(
+  "../Data/International_Report_Passengers.csv",
+  header = TRUE,
+  stringsAsFactors = FALSE
+)
+
+# International Report Passengers Dataframe Manipulation 
+
+# Calculate total passengers each year 
+Passengers_Report <- International_Report_Passengers %>% 
+  filter(Month <= 3) %>% 
+  group_by(Year) %>% 
+  summarize(total_passengers = sum(Total)) 
+
+# Calculate percentage of passengers each year compared to total passengers of all years 
+Passengers_Report <- Passengers_Report %>% 
+  mutate(percent_total = round((total_passengers / sum(total_passengers) * 100), 2)) %>%   
+  mutate(shift = lead(Passengers_Report$total_passengers, default = 0)) 
+
+# Calculate percent increase in passengers from the previous year 
+Passengers_Report <- Passengers_Report %>% 
+  mutate(percent_increase = round(((shift - total_passengers) / total_passengers) * 100, 2)) 
+
+Passengers_Report <- Passengers_Report %>% 
+  mutate(percent_increase = lag(Passengers_Report$percent_increase, default = NA)) %>% 
+  select(-shift) %>% 
+  rename(janurary_to_march_each_year = Year)
+
+View(Passengers_Report)
+
+
+
