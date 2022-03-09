a06c3861d2563f94851efb2104821a6c0f396dfe
lattematics <linda71@2uw.edu>
2022-03-08 23:52:42 -0800

    add Chart 2 & Report

--- docs/app_server.R
+++ docs/app_server.R
@@ -5,8 +5,21 @@ library(stringr)
 source("../source/summary.R")
 source("../source/summary_table.R")
 source("../source/chart1.R")
-source("../source/chart2.R")
-source("../source/chart3.R")
+#source("../source/chart3.R")
+
+pass_data <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-Covid-Air-Travels/main/data/International_Report_Passengers.csv", 
+                              stringsAsFactors = FALSE)
+date_filter <- pass_data %>%
+  filter(Month > 0 & Month < 4 & Year > 2009)
+
+time_range <- range(date_filter$Year)
+
+most_pass <- pass_data %>%
+  select(carrier, Total, Year, Month) %>%
+  filter(Year >= 2010) %>%
+  group_by(carrier) %>%
+  summarise(Total = sum(Total))
+
 
 
 server <- function(input, output) { 
@@ -28,19 +41,20 @@ server <- function(input, output) {
     airlines receive."
   })
   
+  # Chart 1
   output$chart1 <- renderPlot({
     if(input$vis1 == "All"){
-        p <- ggplot(data = NULL, 
-               aes(x = Year, y = Total,color = factor(Month))) +
-          geom_line(data = jan_total) + 
-          geom_line(data = feb_total) + 
-          geom_line(data = mar_total) +
-          ggtitle("Total Passengers by Months") +
-          ylab("Total Passenger") +
-          scale_color_discrete(name = "Months", 
-                               label = c("January", "February", "March"))
-        return(p)
-        
+      p <- ggplot(data = NULL, 
+                  aes(x = Year, y = Total,color = factor(Month))) +
+        geom_line(data = jan_total) + 
+        geom_line(data = feb_total) + 
+        geom_line(data = mar_total) +
+        ggtitle("Total Passengers by Months") +
+        ylab("Total Passenger") +
+        scale_color_discrete(name = "Months", 
+                             label = c("January", "February", "March"))
+      return(p)
+      
     } else if(input$vis1 == "January"){
       current_select <- jan_total
     } else if(input$vis1 == "February"){
@@ -59,59 +73,40 @@ server <- function(input, output) {
                            label = input$vis1)
     
     return(p)
-    
-    #FEEDBACK: "Your first chart is great, and I do think that it's one of your 
-    #strongest graphs as it does the best job of showing just how much COVID 
-    #affected flights. For the widgets, I would suggest having the user select 
-    #the range of dates or years they want or certain months"
-    
   })
+  # end of Chart 1
   
-  output$chart2 <- renderPlot({
+  # Chart 2
+  output$chart2 <- renderPlotly({
+    temp <- date_filter %>%
+      filter(Year >= input$time_range[1], Year <= input$time_range[2])
     
-    
-    #FEEDBACK: "Your second chart is great, one thing that I might work on 
-    #would be maybe adding a more descriptive X-axis might help increase the 
-    #clarity to the table, or even a more descriptive title. Another thing 
-    #might be to make sure the user knows what year you are looking at, is it 
-    #only one year, one month, one day, or all the data inside of the data set?"
-    
-  })
-  
-  output$chart3 <- renderPlot({
-    if(input$vis3 == "All carriers"){
-      p <- ggplot(data = avg_passengers, aes(x = Year, y = all)) +
-        geom_bar(stat = "identity", fill = "olivedrab3") +
-        labs(y = "Average number of passengers", x = "Year") +
-        ggtitle("Average number of flight passengers by year")
-    } else if(input$vis3 == "American Airlines"){
-      p <- ggplot(data = avg_passengers, aes(x = Year, y = aa)) +
-        geom_bar(stat = "identity", fill = "firebrick3") +
-        labs(y = "Average number of passengers", x = "Year") +
-        ggtitle("Average number of American Airlines' passengers by year")
-    } else if(input$vis3 == "Delta Airlines") {
-      p <- ggplot(data = avg_passengers, aes(x = Year, y = dl)) +
-        geom_bar(stat = "identity", fill = "dodgerblue2") +
-        labs(y = "Average number of passengers", x = "Year") +
-        ggtitle("Average number of Delta Airlines' passengers by year")
-    } else if(input$vis3 == "Southwest Airlines") {
-      p <- ggplot(data = avg_passengers, aes(x = Year, y = wn)) +
-        geom_bar(stat = "identity", fill = "darkgoldenrod1") +
-        labs(y = "Average number of passengers", x = "Year") +
-        ggtitle("Average number of Southwest Airlines' passengers by year")
-    } else if(input$vis3 == "United Airlines") {
-      p <- ggplot(data = avg_passengers, aes(x = Year, y = ua)) +
-        geom_bar(stat = "identity", fill = "purple4") +
-        labs(y = "Average number of passengers", x = "Year") +
-        ggtitle("Average number of United Airlines' passengers by year")
-    } else {
-      p <- ggplot(data = avg_passengers, aes(x = Year, y = cz)) +
-        geom_bar(stat = "identity", fill = "deepskyblue3") +
-        labs(y = "Average number of passengers", x = "Year") +
-        ggtitle("Average number of China Southern Airlines' passengers by year")
+    avg_plot <- function(temp,  search = "", 
+                         xvar = "Year", yvar = "Total") {
+      xmax <- max(temp[,xvar]) + 1
+      ymax <- max(temp[,yvar]) 
+      
+      data <- temp %>% 
+        filter(grepl(search, carrier))
+      
+      p <- plot_ly(x = data[, xvar], y = data[, yvar], 
+                   mode = "markers", 
+                   marker = list(
+                     opacity = .5, 
+                     size = 10, 
+                     color = "Month"
+                   )) %>% 
+        layout(xaxis = list(range = c(2009.5, xmax), title = xvar), 
+               yaxis = list(range = c(0, ymax), 
+                            title = paste0("Totals for ", input$search))
+        )
+      return(p)
     }
-    return(p)
-
+    
+    return(avg_plot(temp, input$search))
   })
+  # end of Chart 2
+  
+
 }
 
--- docs/app_ui.R
+++ docs/app_ui.R
@@ -21,6 +21,147 @@ home_panel <- tabPanel(
   )
 )
 
+report_main_content <- mainPanel(
+  h3("Author: By: Monica Dao (xmtdx@uw.edu), John Oh (jho12@uw.edu), 
+     Garima Thapa (gt35@uw.edu), Linda Giang (linda71@uw.edu)"),
+  h4(em("INFO-201: Technical Foundations of Informatics - 
+        The Information School - University of Washington WINTER 2022")),
+  h4(em("Abstract")),
+  p("Our main question is how air travel has changed over the years and how 
+    covid has impacted the lives of the people that travel. This question is 
+    important because covid has caused many losses for airlines and changed the 
+    way people interact with one another. To help answer this question, we will 
+    find details on the changes in passengers that the airlines receive."),
+  h4(em("Introduction")),
+  p("The coronavirus that has rapidly spread since early 2020 caused many to be 
+    apprehensive towards crowds of people. Airplanes are examples of this, 
+    being an enclosed space that seats many people for an average of 3 hours. 
+    Due to such concerns, airlines have been seeing a decline in business since 
+    March of 2020, with many countries adding travel bans and restrictions. 
+    With the introduction of vaccines and safety practices, many are starting 
+    to travel by air again. An initial study conducted by the Harvard T.H. Chan 
+    School of Public Health concluded that “there is a low risk of COVID-19 
+    transmission on aircraft” (Barnes, 2020) as of October 2020. However, with 
+    the new covid Omicron variant, the recovery of airline businesses may 
+    continue to see limited passengers. There are articles where an airline 
+    medical advisor predicted that Omicron will make it “twice or even three 
+    times more likely to catch Covid-19 during a flight” (Whitley, 2021). The 
+    trends of passengers may inversely follow the number of cases, but with the 
+    effectiveness of the vaccines and the addition of the booster, this may not 
+    have to be the case."), 
+  h4(em("Design Situation")), 
+  h5("Framing the topic of concern"), 
+  p("Air travel is one of the most prevalent ways of traveling, and it has 
+    impacted the way people plan long-term and short-term travel in the United 
+    States. But in 2020, Covid-19 has changed the behaviors of people that 
+    existed since airlines have become commercialized, which shows the severity 
+    Covid-19 had on people’s lives. Not only did it change the behavior of 
+    people on using commercial airlines, but it also changed the behavior of 
+    airlines, “reducing Domestic departures declined 71.5% in May 2020 as 
+    compared to the previous year” (Hotle, 2020)."),
+  h5("Human Values"),
+  p("There are many reasons behind deciding to opt-out of airline traveling, 
+    which one may argue is due to safety concerns. However, this decision goes 
+    beyond just safety and is related to unemployment, uncertainty, and 
+    psychological hindrance due to Covid-19. For example, in 2020 unemployment 
+    in the U.S. rose about 100% from the previous year (Bureau of Labor 
+    Statistics, 2021). Such data shows the priority of the people and their 
+    values in safety, which originates from deaths and over-hospitalization 
+    due to Covid-19."),
+  h5("Stakeholders"),
+  p("The direct stakeholders are airports and airlines. Airlines worldwide 
+    suffered a -$137.7 billion in net loss post covid during 2020, showing the 
+    significant impact Covid had on airline profits (Mazareanu, 2021). Any 
+    workers of airports and airlines can suffer financial damage from traveling 
+    decline, making our data an important part of their operation. Indirect 
+    stakeholders are those who wish to travel either for business or personal 
+    reasons, as rising in Covid-19 would limit them from using air traveling as 
+    an option"),
+  h5("Benefits and Harms"),
+  p("A benefit of having Covid-19 and Airtravel history data is its sensitivity 
+    to trends. Both data can be generally analyzed before significant 
+    outbreaks, which means actions can be taken based on an increasing trend 
+    in Covid-19 and decreasing trend in air travel, limiting the chance of 
+    compromising people’s safety when traveling. Airline workers are able to 
+    avoid being in a dangerous situation. However, these data often change 
+    drastically throughout periods, creating unpredictability when actions are 
+    needed. This could create confusion amongst decision-makers in the airline 
+    industry regarding air travel operations, which may contradict policy 
+    makers’ decisions."),
+  h4(em("Research questions")),
+  p("- How has passenger density changed in the various airlines? 
+    Has covid added to this change?"),
+  p("- Are the shifts in passenger density applicable across the airlines?"),
+  p("- What does the average passengers show in these airlines?"),
+  p("Many adjustments had to be made in the daily lives of people including how 
+    they interact with people who live far away with them. With airplanes being 
+    an enclosed space and passengers seated in close proximity, the risks of 
+    covid is increased. Therefore these questions seek to find out how people 
+    have been using the various airlines even before covid, to see how much it 
+    has impacted the lives of the general public. Whether the usage of airlines 
+    are common and necessary enough to take on the increased risks of covid or 
+    if staying connected without being physically present is a decent 
+    replacement to using airlines. The changes in usage of these various 
+    airlines can also seek to optimize the usage of planes and decrease the 
+    greenhouse gas produced. In 2019, it was seen that 29% of carbon pollution 
+    came from the transportation sector (Sources of Greenhouse Gas Emissions, 
+    2021)."),
+  h4(em("The Dataset")), 
+  p("The dataset is U.S. International Air Traffic data(1990-2020) from Kaggle 
+    on airport and airline traffic by US and international carriers. The data, 
+    held in the U.S. Department of Transportation website, is from the U.S. 
+    International Air Passenger and Freight Statistics Report page. This 
+    particular dataset is on passengers and holds information on the number of 
+    passengers each month and year between two airports serviced by a specific 
+    airline. This report is part of the T-100 program and it was developed to 
+    allow the public to have access to data on flights internationally. The 
+    dataset reports are updated quarterly, and the site has multiple datasets 
+    around Passenger, Freight, Departures, Seats. The effect of the pandemic on 
+    traffic can be analyzed in the 2020 report."),
+  p("The T-100 program by the USDOT began on January 1, 1990. It produces 
+    reports of foreign airlines operating to and from the United States and 
+    traffic reports of both domestic and international operations of U.S. 
+    airlines. The reports are submitted by airlines to the Department’s 
+    Office of Airline Information of the Bureau of Transportation Statistics. 
+    There is a rule that states that data must be confidential for 6 months 
+    before reporting."),
+  p("This dataset is very large and has over 680k rows and 16 columns. Columns 
+    are labeled as: data_dte (data date), Year (data year), Month (Data month), 
+    usg_apt_id (US Gateway airport ID), usg_apt (US Gateway airport code), 
+    usg_wac (US Gateway World area code, to represent a geographic territory), 
+    fg_apt_id (Foreign Gateway Airport ID), fg_apt (Foreign Gateway Airport 
+    code), fg_wac (Foreign Gateway world area code), airlineid (Airline ID), 
+    carrier (IATA assigned air carrier code), carriergroup (Carrier Group Code 
+    – 1 denotes US domestic air carrier, 0 is foreign), type (Passengers), 
+    Scheduled (metric flown by scheduled service operations), Charter (metric 
+    flown by charter operations), and Total (total metric flown by scheduled 
+    service and charter operations)."),
+  p("This dataset contains all nonstop commercial passenger traffic traveling 
+    between international points and U.S. airports. The data covers the 
+    timeline between January 1, 1990 to March 1, 2020. This dataset does not 
+    cover all aspects of international travel as nonstop traffic to and from 
+    U.S. cities that serve as international gateway will have high numbers."),
+  p("It should be noted that some traffic continues on a connecting flight due 
+    to most international passengers having at least one connecting flight 
+    before reaching their final destination. U.S. carriers also serve some 
+    international points through an international connection only. Although 
+    the data can seem to show that no U.S. carrier serves an international 
+    point, the carrier traffic is first moving through a connecting city. 
+    There are influences of code-share agreements on this data, and this 
+    agreement makes it common for passengers to fly on an aircraft operated 
+    by an airline different from the one they bought their ticket from."),
+)
+  
+
+report_panel <- tabPanel(
+  "Report",
+  titlePanel("The Coronavirus’s Effect on Air Travels and the Implications"), 
+  sidebarLayout(
+    NULL,
+    report_main_content
+  )
+)
+
 chart1_main_content <- mainPanel(
   plotOutput("chart1")
 )
@@ -47,26 +188,43 @@ chart1_panel <- tabPanel(
   )
 )
 
+pass_data <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-Covid-Air-Travels/main/data/International_Report_Passengers.csv", 
+                              stringsAsFactors = FALSE)
+date_filter <- pass_data %>%
+  filter(Month > 0 & Month < 4 & Year > 2009)
+
+time_range <- range(date_filter$Year)
+
 chart2_main_content <- mainPanel(
-  plotOutput("chart2")
+  plotlyOutput("chart2")
 )
 
 chart2_sidebar_content <- sidebarPanel(
-  selectInput(
-    "vis2",
-    label = "Carriers",
-    choices = list(
-      "All",
-      "January", 
-      "February", 
-      "March"
-    )
+  selectInput("search", label = "carrier", 
+              choice = list("American Airlines Inc." = "AA",
+                            "United Airlines" = "UA",
+                            "Delta Air Lines, Inc." = "DL",
+                            "British Airways" = "BA",
+                            "Jetblue Airways Corporation" = "B6",
+                            "Lufthansa" = "LH",
+                            "Air Canada" = "AC",
+                            "WestJet" = "WS",
+                            "US Airways" = "US",
+                            "Société Air France" = "AF"), 
+              selected = "American Airlines Inc."
+  ), 
+  sliderInput(
+    inputId = "time_range",
+    label = "Year",
+    min = time_range[1],
+    max = time_range[2],
+    value = time_range
   )
 )
 
 chart2_panel <- tabPanel(
   "Chart 2",
-  titlePanel("Passenger by Carrier"),
+  titlePanel("Total Passengers by Carrier"),
   sidebarLayout(
     chart2_sidebar_content,
     chart2_main_content
@@ -74,7 +232,7 @@ chart2_panel <- tabPanel(
 )
 
 chart3_main_content <- mainPanel(
-  plotOutput("chart3")
+   #plotOutput("chart3")
 )
 
 chart3_sidebar_content <- sidebarPanel(
@@ -173,6 +331,7 @@ conclusion_panel <- tabPanel(
 ui <- navbarPage(
   "Info 201",
   home_panel,
+  report_panel,
   chart1_panel, 
   chart2_panel,
   chart3_panel,
